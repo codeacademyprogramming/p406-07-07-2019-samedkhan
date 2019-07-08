e23ae9c3851e732871541a77bb0e04e4591e9f83
samedkhan <samed.khankishiev@moe.minenergy.gov.az>
2019-07-08 19:23:23 +0400

    Homework is beginning

---
+++ HOMEWORK/assets/JS/script.js
@@ -0,0 +1,27 @@
+// Student properties:
+// Name,
+// Surname,
+// Age,
+// PhoneNumber
+
+function Student(name, surname, age, phoneNumber){
+    this.name= name;
+    this.surname = surname;
+    this.age = age;
+    this.phoneNumber = phoneNumber;
+}
+
+
+// Teacher properties:
+// Name,
+// Surname,
+// Age,
+// Educations -> array
+// Experience -> array
+
+function Teacher (name, surname, age, educations, experience){
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    
+}
